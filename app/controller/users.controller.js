4e485c6f296815dd83a76772939377d083dc224b
Noman Javed <NomanJaved2011@gmail.com>
2019-05-17 15:37:12 +0500

    update with ejs working

--- app/controller/users.controller.js
+++ app/controller/users.controller.js
@@ -1,6 +1,8 @@
 const userModel = require('../model/user.model');
 
 exports.createUser = (req, res) => {
+    console.log(req.body);
+    
     const user = new userModel(req.body);
     user.save().then(data=>{
         res.send({
@@ -14,27 +16,45 @@ exports.getUsers = (req, res) => {
     userModel.find().then(data => {
         res.render('userList', {
             data: data
-        })
+        })        
     })
 }
+
 
 exports.getSingleUser = (req, res) => {
     let id = req.params.id;
     userModel.findById(id).then(data => {
-        res.send({
-            success:true,
-            data:data
+        // console.log("Hello get single user");
+        res.render('editUser', {
+            data: data
         })
+        // res.send({
+        //     success:true,
+        //     data_edit:data
+        // })
     })
 }
 
 exports.updateUser = (req, res) => {
-    let id = req.params.id;
-    let data = req.body;
+    console.log(req.body);
+    let id = req.body.id;
+    var data = {        
+        user_name : req.body.user_name,
+        email : req.body.email,
+        active : req.body.status
+    }
+    console.log(data);    
+    
     userModel.findByIdAndUpdate(id, data).then(data => {
         res.send({
             success:true,
-            data:data
+            data:data,
+            message: 'User updated.'
+        })
+    }).catch(err => {
+        res.send({
+            success:true,
+            message:'update Error'
         })
     })
 }
--- app/routes/users.route.js
+++ app/routes/users.route.js
@@ -5,7 +5,8 @@ module.exports = (app) => {
     app.post('/create', userController.createUser);
     app.get('/users', userController.getUsers);
     app.get('/user/:id', userController.getSingleUser);
-    app.put('/update/:id', userController.updateUser);
+    // app.put('/update/:id', userController.updateUser);
+    app.post('/update/:id', userController.updateUser);
     app.delete('/delete/:id', userController.deleteUser);
 
 }
